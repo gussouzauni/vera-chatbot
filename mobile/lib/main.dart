7a54928cf7879f687e18c176b2729c3751147ecf
gussouzauni <gustavo10ssouza@gmail.com>
2020-04-25 23:00:11 -0300

    init project

---
+++ mobile/lib/main.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class MainDart extends StatefulWidget {
+  @override
+  _MainDartState createState() => _MainDartState();
+}
+
+class _MainDartState extends State<MainDart> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Challenge VTex'),
+      ),
+    );
+  }
+}
